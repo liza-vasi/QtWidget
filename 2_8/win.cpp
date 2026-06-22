f9986369aa01be7c2b8be7936898e62aed9dea8d
liza_vasi <viki.sova@yandex.ru>
2026-06-22 20:50:07 +0700

    change codec to fromUtf8

--- 2_8/win.cpp
+++ 2_8/win.cpp
@@ -2,14 +2,14 @@
 
 Win::Win(QWidget *parent):QWidget(parent)
 {
-    codec = QTextCodec::codecForName("Windows-1251");
-    this->setWindowTitle(codec->toUnicode("Счетчик"));
-    label1 = new QLabel(codec->toUnicode("Cчет по 1"),this);
-    label2 = new QLabel(codec->toUnicode("Cчет по 5"),this);
+    //codec = QTextCodec::codecForName("Windows-1251");
+    this->setWindowTitle(QString::fromUtf8("Счетчик"));
+    label1 = new QLabel(QString::fromUtf8("Cчет по 1"),this);
+    label2 = new QLabel(QString::fromUtf8("Cчет по 5"),this);
     edit1 = new Counter("0",this);
     edit2 = new Counter("0",this);
     calcbutton=new QPushButton("+1",this);
-    exitbutton=new QPushButton(codec->toUnicode("Выход"),this);
+    exitbutton=new QPushButton(QString::fromUtf8("Выход"),this);
     QHBoxLayout *layout1 = new QHBoxLayout();
     layout1->addWidget(label1);
     layout1->addWidget(label2);
@@ -17,13 +17,13 @@ Win::Win(QWidget *parent):QWidget(parent)
     layout2->addWidget(edit1);
     layout2->addWidget(edit2);
     QHBoxLayout *layout3 = new QHBoxLayout();
-    layout3->addWidget(culcbutton);
+    layout3->addWidget(calcbutton);
     layout3->addWidget(exitbutton);
     QVBoxLayout *layout4 = new QVBoxLayout(this);
     layout4->addLayout(layout1);
     layout4->addLayout(layout2);
     layout4->addLayout(layout3);
-    // связь сигнала нажатия кнопки и слота закрытия окна
+
     connect(calcbutton,SIGNAL(clicked(bool)),
             edit1,SLOT(add_one()));
     connect(edit1,SIGNAL(tick_signal()),
